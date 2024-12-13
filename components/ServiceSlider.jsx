92eae1250962eca9b4b64ac1223d16ebdcbaf6d8
RzayevUmid0023 <umid.official.main@gmail.com>
2024-12-13 15:25:33 +0400

    .
    .

---
+++ components/ServiceSlider.jsx
@@ -0,0 +1,94 @@
+import {
+  RxCrop,
+  RxPencil2,
+  RxDesktop,
+  RxReader,
+  RxRocket,
+  RxArrowTopRight,
+} from "react-icons/rx";
+import { FreeMode, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+const serviceData = [
+  {
+    Icon: RxCrop,
+    title: "Frontend Dev",
+    description: "Building responsive web apps with React, Next.js, and Tailwind CSS.",
+  },
+  {
+    Icon: RxPencil2,
+    title: "Modern Design",
+    description: "Creating sleek, responsive, modern designs to enhance user experience.",
+  },
+  {
+    Icon: RxDesktop,
+    title: "Web Development",
+    description: "Developing high-performance applications with React and Redux.",
+  },
+  {
+    Icon: RxReader,
+    title: "API Integration",
+    description: "Integrating third-party APIs for enhanced functionality and real-time data.",
+  },
+  {
+    Icon: RxRocket,
+    title: "Performance Opt.",
+    description: "Optimizing web performance to reduce load times and improve UX.",
+  },
+];
+
+
+
+const ServiceSlider = () => {
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[FreeMode, Pagination]}
+      freeMode
+      className="h-[240px] sm:h-[340px]"
+    >
+      {serviceData.map((item, i) => (
+        <SwiperSlide key={i}>
+          <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
+            {/* icon */}
+            <div className="text-4xl text-accent mb-4">
+              <item.Icon aria-hidden />
+            </div>
+
+            {/* title & description */}
+            <div className="mb-8">
+              <div className="mb-2 text-lg">{item.title}</div>
+              <p className="max-w-[350px] leading-normal">{item.description}</p>
+            </div>
+
+            {/* arrow */}
+            <div className="text-3xl">
+              <RxArrowTopRight
+                className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300"
+                aria-hidden
+              />
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default ServiceSlider;
